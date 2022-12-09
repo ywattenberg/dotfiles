9ac3ed666163defe7394dd3759d27be42b3505d3
Yannick Wattenberg <wattenberg@mailbox.org>
2022-12-09 23:46:50 +0100

    add script to get layout

---
+++ waybar/modules/layout.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LAYOUT=$(swaymsg -t get_inputs | jq 'map(select(has("xkb_active_layout_name")))[0].xkb_active_layout_name')
+
+if [[ $LAYOUT == "\"English (US)\"" ]]; then
+    text="us"
+else
+    text="de"
+fi
+
+echo -e "{\"text\":\"$text\"}"
