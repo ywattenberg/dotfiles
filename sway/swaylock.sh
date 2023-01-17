6387288579c144e14bcb4ce02bfd699a180b1986
Yannick Wattenberg <wattenberg@mailbox.org>
2023-01-17 11:06:59 +0100

    add config for sworkstyle and configure sway accordingly

--- sway/swaylock.sh
+++ sway/swaylock.sh
@@ -8,7 +8,7 @@
         --effect-blur 7x5 \
         --effect-vignette 0.5:0.5 \
         --ring-color bb00cc \
-        --key-hl-color 880033 \
+        --key-hl-color 5ddd74 \
         --line-color 00000000 \
         --inside-color 00000088 \
         --separator-color 00000000 \
