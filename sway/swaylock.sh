a59d4a706a277e0686de3bb34c44bc8d28a48597
Yannick Wattenberg <wattenberg@ywattenberg.com>
2023-07-27 10:24:04 +0200

    changes

--- sway/swaylock.sh
+++ sway/swaylock.sh
@@ -5,12 +5,12 @@
         --indicator \
         --indicator-radius 100 \
         --indicator-thickness 7 \
-        --effect-blur 7x5 \
+        --effect-blur 9x7 \
         --effect-vignette 0.5:0.5 \
         --ring-color bb00cc \
         --key-hl-color 5ddd74 \
         --line-color 00000000 \
         --inside-color 00000088 \
         --separator-color 00000000 \
-        --grace 2 \
+        --grace 10 \
         --fade-in 0.2
